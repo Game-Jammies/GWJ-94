5418e74c723ac01db00e8b25d3cc1fe6122a830d
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2026-06-14 00:54:32 -0500

    timer tweaks

--- scenes/timer/timer.gd
+++ scenes/timer/timer.gd
@@ -3,18 +3,18 @@ extends Panel
 var time: float = 0.0
 var minutes: int = 0
 var seconds: int = 0
-var msec: int = 0
+var msec: float = 0
 
 func _process(delta) -> void:
 	time += delta
-	msec = fmod(time, 1) * 100
-	seconds = fmod(time, 60)
-	minutes = fmod(time, 3600) / 60
-	$Minutes.text = "%02d:" % minutes
+	msec = fmod(time, 1) * 1000.0
+	seconds = int(time) % 60
+	minutes = int(time / 60) % 60
+	$Minutes.text = "%d:" % minutes
 	$Seconds.text = "%02d" % seconds
 
 func stop() -> void:
 	set_process(false)
 
 func get_time_formatted() -> String:
-	return "%02d:%02d" % [minutes, seconds]
+	return "%d:%02d" % [minutes, seconds]
